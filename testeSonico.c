18c8f2168a425da8fc8a310d1a697047d0d77e6a
Nicolas Ramos <77813246+nicolasURamos@users.noreply.github.com>
2023-01-25 15:02:35 -0300

    Create testeSonico.c

---
+++ testeSonico.c
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <Ultrasonic.h>
+
+const int trigPin = A1;
+const int echoPin = A0;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(500);
+}
